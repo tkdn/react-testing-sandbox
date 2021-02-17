84f6d305c22aeaf99bff230eb48b6ef95fbeeaa7
tkdn <tkdnation@gmail.com>
2021-02-17 18:13:54 +0900

    fix: act 関数の誤った利用を修正

--- src/components/ReactQuery/ReactQuery.test.tsx
+++ src/components/ReactQuery/ReactQuery.test.tsx
@@ -24,32 +24,24 @@ describe("ReactQuery", () => {
     const { asFragment } = render(<WrappedReactQuery size={5} />)
     screen.getByText("loading...")
     expect(asFragment()).toMatchSnapshot()
-    await act(async () => {
-      await waitForElementToBeRemoved(() => screen.getByText("loading..."))
-    })
+    await waitForElementToBeRemoved(() => screen.getByText("loading..."))
   })
   test("render:pokemons", async () => {
     const { asFragment } = render(<WrappedReactQuery size={5} />)
     screen.getByText("loading...")
-    await act(async () => {
-      await waitForElementToBeRemoved(() => screen.getByText("loading..."))
-    })
+    await waitForElementToBeRemoved(() => screen.getByText("loading..."))
     expect(asFragment()).toMatchSnapshot()
   })
   test("render:no pokemon", async () => {
     const { asFragment } = render(<WrappedReactQuery size={0} />)
-    await act(async () => {
-      await waitFor(() => screen.getByText("no pokemon"))
-    })
+    await waitFor(() => screen.getByText("no pokemon"))
     expect(asFragment()).toMatchSnapshot()
   })
   test("error", async () => {
     const spy = jest.spyOn(console, "error")
     spy.mockImplementation(() => void 0)
     render(<WrappedReactQuery size={400} />)
-    await act(async () => {
-      await waitFor(() => screen.getByText("http status: 400"))
-    })
+    await waitFor(() => screen.getByText("http status: 400"))
     spy.mockRestore()
   })
 })
