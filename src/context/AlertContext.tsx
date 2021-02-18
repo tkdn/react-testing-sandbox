335df92e9424c6c57d4d312cf0ef3a5e9564eb1b
tkdn <tkdnation@gmail.com>
2021-02-18 16:19:23 +0900

    feat: Context を利用してアラートを出すサンプル

---
+++ src/context/AlertContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useCallback, useContext, useReducer } from "react"
+
+type AlertState = {
+  show: boolean
+  message: string
+}
+type AlertDispatch = {
+  showDispatcher: (message: string) => void
+  hideDispatcher: () => void
+}
+const initialState = {
+  show: false,
+  message: ""
+}
+const alertStateContext = createContext<AlertState>(initialState)
+const alertDispatchContext = createContext<AlertDispatch>({
+  showDispatcher: () => void 0,
+  hideDispatcher: () => void 0
+})
+
+export const useAlertState = () => useContext(alertStateContext)
+export const useAlertDispatch = () => useContext(alertDispatchContext)
+
+export function AlertProvider({ children }: { children: JSX.Element[] }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const showDispatcher = useCallback((message: string) => dispatch(showAlert(message)), [])
+  const hideDispatcher = useCallback(() => dispatch(hideAlert()), [])
+  return (
+    <alertStateContext.Provider value={state}>
+      <alertDispatchContext.Provider value={{ showDispatcher, hideDispatcher }}>
+        {children}
+      </alertDispatchContext.Provider>
+    </alertStateContext.Provider>
+  )
+}
+
+type AlertShowAction = ReturnType<typeof showAlert>
+type AlertHideAction = ReturnType<typeof hideAlert>
+
+function reducer(state: AlertState, action: AlertShowAction | AlertHideAction) {
+  switch (action.type) {
+    case "ui/alert:show":
+      return {
+        ...state,
+        show: true,
+        message: action.payload.message
+      }
+    case "ui/alert:hide":
+      return {
+        ...state,
+        show: false
+      }
+    default:
+      return state
+  }
+}
+
+function showAlert(message: string) {
+  return {
+    type: "ui/alert:show",
+    payload: {
+      message
+    }
+  } as const
+}
+
+function hideAlert() {
+  return {
+    type: "ui/alert:hide"
+  } as const
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
 
+import { Alert } from "~/components/Alert"
 import { Analytics } from "~/components/Analytics"
 import { AsyncCounter } from "~/components/AsyncCounter"
+import { Button } from "~/components/Button"
 import { Counter } from "~/components/Counter"
 import { Header } from "~/components/Header"
+import { AlertProvider } from "~/context/AlertContext"
 
 export default function Index() {
   return (
-    <>
+    <AlertProvider>
+      <Alert />
       <nav>
         <ul>
           <li>
@@ -31,9 +35,12 @@ export default function Index() {
       <h1>AsyncCounter</h1>
       <AsyncCounter />
       <hr />
+      <h1>Alert Button Context</h1>
+      <Button />
+      <hr />
       <h1>Analytics</h1>
       <pre>null</pre>
       <Analytics id="123" role="admin" />
-    </>
+    </AlertProvider>
   )
 }
